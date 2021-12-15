833ce4df245f706ef0e14234334a55f0f2617d81
mike seibel <mikes@washington.edu>
2021-12-15 09:58:48 -0800

    startup telemetry

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 set -e
 
+# telemetry
+echo "startup telemetry"
+echo -n "whoami: "
+whoami
+
+echo -n "ls /scripts:"
+ls -l /scripts
+
 if [ ! -d '/var/spool/postfix/etc' ]
 then
+  echo "exec: mkdir /var/spool/postfix/etc"
   mkdir /var/spool/postfix/etc
 fi
 
 if [ ! -e '/var/spool/postfix/etc/services' ]
 then
+    echo "exec: ln -s /etc/services /var/spool/postfix/etc/"
     ln -s /etc/services /var/spool/postfix/etc/
 fi
 
+echo "config files: "
+ls -l /config
+
+echo "/cofig/main.cf"
+cat /config/main.cf
+
+
+echo "Starting postfix"
 /usr/sbin/postfix start-fg
