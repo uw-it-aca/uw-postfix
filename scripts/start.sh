3ec65c708a9404579120870ff2a985f7fb78af96
mike seibel <mikes@washington.edu>
2022-12-21 12:20:16 -0800

    skip unknown env vars

--- scripts/start.sh
+++ scripts/start.sh
@@ -43,7 +43,7 @@ do
     awk '{
            while (match($0,"[$]{[^}]*}")) {
              var = substr($0,RSTART+2,RLENGTH -3)
-             gsub("[$]{"var"}",ENVIRON[var])
+             if (var in ENVIRON) gsub("[$]{"var"}",ENVIRON[var])
            }
          }1' < $CFG_TEMPLATE_IN  > ${CONFIG_FILE_DIRECTORY}/$(basename -s .${CONFIG_TEMPLATE_EXTENSION} $CFG_TEMPLATE_IN).${CONFIG_FILE_EXTENSION}
 done
